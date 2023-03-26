4c424c17a2912fd94098fb03e6c445fe49d6ac96
Sahrial-alam34 <sunnykcp01@gmail.com>
2023-03-26 22:05:18 +0600

    added the header

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Header from './components/Header/Header'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+     <Header></Header>
+    </div>
+  )
+}
+
+export default App
