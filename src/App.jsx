938fc1ee460c1d308eb070d2836fb75ef8900567
Sahrial-alam34 <sunnykcp01@gmail.com>
2023-03-26 22:59:23 +0600

    load product

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Header from './components/Header/Header'
+import Shop from './components/Shop/Shop'
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,6 +11,8 @@ function App() {
   return (
     <div className="App">
      <Header></Header>
+     <Shop></Shop>
+   
     </div>
   )
 }
