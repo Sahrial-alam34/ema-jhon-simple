7f79b0018621ef08c291ba07eca07f0873c153e2
Sahrial-alam34 <sunnykcp01@gmail.com>
2023-03-28 00:34:42 +0600

    handle quantity from local storage to cart

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -48,7 +48,22 @@ const Shop = () => {
 
     const handleAddToCart = (product) => {
         // cart.push(product); not possible , immutable
-        const newCart = [...cart, product];
+        let newCart = [];
+        // const newCart = [...cart, product];
+        // if product doesn't exist in the cart , then set quantity = 1
+        // if exist update quantity by 1
+        const exists = cart.find(pd=>pd.id === product.id);
+        if(!exists){
+            product.quantity = 1;
+            newCart = [...cart, product]
+        }
+        else{
+            exists.quantity = exists.quantity + 1;
+            const remaining = cart.filter(pd=> pd.id !== product.id)
+                newCart = [...remaining, exists];
+            
+        }
+
         setCart(newCart);
         addToDb(product.id)
     }
